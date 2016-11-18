b562718c11c85cbeff1ad59cafd29e06af62e1c3
Patrick Niklaus <patrick.niklaus@student.kit.edu>
2016-11-18 12:45:27 +0000

    Connect to google play services

--- app/src/main/java/de/tr0llhoehle/disease/LocationTracker.java
+++ app/src/main/java/de/tr0llhoehle/disease/LocationTracker.java
@@ -1,16 +1,35 @@
 package de.tr0llhoehle.disease;
 
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.os.Bundle;
 
-public class LocationTracker extends Service {
+public class LocationTracker extends Service implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener{
     private GoogleApiClient googleClient;
 
     public LocationTracker() {
-        this.googleClient = new GoogleApiClient.Builder(this).addApi()
+        this.googleClient = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+    }
+
+    @Override
+    public void onConnectionSuspended(int value) {
     }
 
     @Override
