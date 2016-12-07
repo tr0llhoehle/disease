3172ab1da23a09535e850a606a568500dd433359
Patrick Niklaus <patrick.niklaus@student.kit.edu>
2016-12-07 23:37:52 +0000

    Fix typo

--- backend/scripts/deploy.sh
+++ backend/scripts/deploy.sh
@@ -12,7 +12,7 @@ DEPLOY_TYPE=${1:-"Staging"}
 PORT=5000
 PORT_SSL=5001
 
-RUN_CMD="docker run -d -p ${PORT}:${PORT} -p ${PORT_SSL}:${PORT_SSL} -v /home/patrick/disease:/var/db -v /home/patrick/disease.tr0llhoehle.de:/var/ssl --env APP_DB=${APP_DB} --env APP_CERT=${APP_CERT} --evn APP_KEY=${APP_KEY} --restart=on-failure ${IMAGE} npm run start"
+RUN_CMD="docker run -d -p ${PORT}:${PORT} -p ${PORT_SSL}:${PORT_SSL} -v /home/patrick/disease:/var/db -v /home/patrick/disease.tr0llhoehle.de:/var/ssl --env APP_DB=${APP_DB} --env APP_CERT=${APP_CERT} --env APP_KEY=${APP_KEY} --restart=on-failure ${IMAGE} npm run start"
 
 sudo docker build -t ${IMAGE} .
 sudo docker save ${IMAGE} | bzip2 | pv | ssh tr0llhoehle.de 'bunzip2 | docker load'
